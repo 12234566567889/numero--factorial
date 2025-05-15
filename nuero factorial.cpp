90148a06b022b23b36068128bd7979e77911fdc4
12234566567889 <caterina.viso@hospitalitaliano.org.ar>
2025-05-14 22:29:08 -0300

    Agregar archivos de proyecto.

---
+++ nuero factorial.cpp
@@ -0,0 +1,32 @@
+// nuero factorial.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    int n, i;
+    double factorial = 1;
+
+	std::cout << "Ingrese un número: ";
+    std::cin >> n;
+    if (n < 0) {
+        std::cout << "Error! El factorial de un número negativo no existe." << std::endl;
+    } else {
+        for (i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        std::cout << "El factorial de " << n << " es " << factorial << std::endl;
+    }
+	return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
